9171fb27e9dda67398b748bfb295d69b44ede2d7
Swedz <me@swedz.net>
2024-06-07 02:20:13 -0400

    Make SortOrder comparable

--- tesseract-neoforge/src/main/java/net/swedz/tesseract/neoforge/registry/SortOrder.java
+++ tesseract-neoforge/src/main/java/net/swedz/tesseract/neoforge/registry/SortOrder.java
@@ -1,6 +1,12 @@
 package net.swedz.tesseract.neoforge.registry;
 
-public record SortOrder(int ordinal)
+public record SortOrder(int ordinal) implements Comparable<SortOrder>
 {
 	public static final SortOrder UNSORTED = new SortOrder(Integer.MAX_VALUE);
+	
+	@Override
+	public int compareTo(SortOrder other)
+	{
+		return Integer.compare(ordinal, other.ordinal());
+	}
 }
