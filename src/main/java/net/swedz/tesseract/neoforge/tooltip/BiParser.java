8399f8125ebfdb667d007008fe3cae8bbbb80cbd
Swedz <me@swedz.net>
2024-08-22 01:29:26 -0400

    Add withStyle override to biparser

--- src/main/java/net/swedz/tesseract/neoforge/tooltip/BiParser.java
+++ src/main/java/net/swedz/tesseract/neoforge/tooltip/BiParser.java
@@ -1,6 +1,7 @@
 package net.swedz.tesseract.neoforge.tooltip;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 
 import java.util.Map;
 
@@ -11,5 +12,11 @@ public interface BiParser<A, B> extends Parser<Map.Entry<A, B>>
 	default Component parse(Map.Entry<A, B> value)
 	{
 		return this.parse(value.getKey(), value.getValue());
+	}
+	
+	@Override
+	default BiParser<A, B> withStyle(Style style)
+	{
+		return (a, b) -> this.parse(a, b).copy().setStyle(style);
 	}
 }
