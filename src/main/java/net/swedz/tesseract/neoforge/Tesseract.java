d9c804dcd0cc3c2680a31de7903b84dd35e8cbdb
Swedz <me@swedz.net>
2024-08-25 00:08:12 -0400

    Add armor item interfaces

--- src/main/java/net/swedz/tesseract/neoforge/Tesseract.java
+++ src/main/java/net/swedz/tesseract/neoforge/Tesseract.java
@@ -1,10 +1,18 @@
 package net.swedz.tesseract.neoforge;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.neoforged.neoforge.event.entity.living.LivingEquipmentChangeEvent;
+import net.neoforged.neoforge.event.tick.EntityTickEvent;
 import net.swedz.tesseract.neoforge.datagen.client.LanguageDatagenProvider;
+import net.swedz.tesseract.neoforge.item.ArmorTickHandler;
+import net.swedz.tesseract.neoforge.item.ArmorUnequippedHandler;
 import net.swedz.tesseract.neoforge.proxy.ProxyManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,5 +36,33 @@ public final class Tesseract
 		
 		bus.addListener(GatherDataEvent.class, (event) ->
 				event.getGenerator().addProvider(event.includeClient(), new LanguageDatagenProvider(event)));
+		
+		NeoForge.EVENT_BUS.addListener(EntityTickEvent.Pre.class, (event) ->
+		{
+			if(event.getEntity() instanceof LivingEntity entity)
+			{
+				for(EquipmentSlot slot : EquipmentSlot.values())
+				{
+					if(slot.isArmor())
+					{
+						ItemStack stack = entity.getItemBySlot(slot);
+						if(!stack.isEmpty() &&
+						   stack.getItem() instanceof ArmorTickHandler handler)
+						{
+							handler.armorTick(entity, slot, stack);
+						}
+					}
+				}
+			}
+		});
+		
+		NeoForge.EVENT_BUS.addListener(LivingEquipmentChangeEvent.class, (event) ->
+		{
+			if(!event.getFrom().isEmpty() &&
+			   event.getFrom().getItem() instanceof ArmorUnequippedHandler handler)
+			{
+				handler.onUnequipArmor(event.getEntity(), event.getSlot(), event.getFrom(), event.getTo());
+			}
+		});
 	}
 }
