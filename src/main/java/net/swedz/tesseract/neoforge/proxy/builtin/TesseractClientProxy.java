eae1afecbcd872d761b900ee789053265aed0588
Swedz <me@swedz.net>
2024-08-29 04:38:56 -0400

    Add server and find user methods to the base proxy

--- src/main/java/net/swedz/tesseract/neoforge/proxy/builtin/TesseractClientProxy.java
+++ src/main/java/net/swedz/tesseract/neoforge/proxy/builtin/TesseractClientProxy.java
@@ -2,9 +2,14 @@ package net.swedz.tesseract.neoforge.proxy.builtin;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.swedz.tesseract.neoforge.proxy.ProxyEntrypoint;
 import net.swedz.tesseract.neoforge.proxy.ProxyEnvironment;
+
+import java.util.Optional;
 
 @ProxyEntrypoint(environment = ProxyEnvironment.CLIENT)
 public class TesseractClientProxy extends TesseractProxy
@@ -19,6 +24,26 @@ public class TesseractClientProxy extends TesseractProxy
 	public Player getClientPlayer()
 	{
 		return Minecraft.getInstance().player;
+	}
+	
+	@Override
+	public Optional<Player> findUserWithItem(EquipmentSlot slot, ItemStack stack)
+	{
+		if(Minecraft.getInstance().isSameThread())
+		{
+			for(AbstractClientPlayer player : Minecraft.getInstance().level.players())
+			{
+				if(player.getItemBySlot(slot) == stack)
+				{
+					return Optional.of(player);
+				}
+			}
+			return Optional.empty();
+		}
+		else
+		{
+			return super.findUserWithItem(slot, stack);
+		}
 	}
 	
 	@Override
--- src/main/java/net/swedz/tesseract/neoforge/proxy/builtin/TesseractProxy.java
+++ src/main/java/net/swedz/tesseract/neoforge/proxy/builtin/TesseractProxy.java
@@ -1,12 +1,55 @@
 package net.swedz.tesseract.neoforge.proxy.builtin;
 
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerAboutToStartEvent;
+import net.neoforged.neoforge.event.server.ServerStoppedEvent;
 import net.swedz.tesseract.neoforge.proxy.Proxy;
 import net.swedz.tesseract.neoforge.proxy.ProxyEntrypoint;
+
+import java.util.Optional;
 
 @ProxyEntrypoint
 public class TesseractProxy implements Proxy
 {
+	private MinecraftServer server;
+	
+	@Override
+	public void init()
+	{
+		NeoForge.EVENT_BUS.addListener(ServerAboutToStartEvent.class, (event) -> server = event.getServer());
+		NeoForge.EVENT_BUS.addListener(ServerStoppedEvent.class, (event) -> server = null);
+	}
+	
+	public final boolean hasServer()
+	{
+		return server != null;
+	}
+	
+	public final MinecraftServer getServer()
+	{
+		return server;
+	}
+	
+	public Optional<Player> findUserWithItem(EquipmentSlot slot, ItemStack stack)
+	{
+		if(this.hasServer())
+		{
+			for(ServerPlayer player : server.getPlayerList().getPlayers())
+			{
+				if(player.getItemBySlot(slot) == stack)
+				{
+					return Optional.of(player);
+				}
+			}
+		}
+		return Optional.empty();
+	}
+	
 	public boolean isClient()
 	{
 		return false;
