da2e6d172a769c5fb64c273d0a26b94ee03cf19f
Swedz <me@swedz.net>
2024-10-22 02:58:12 -0400

    Set up basic recipe groups for vanilla crafts

--- src/main/java/net/swedz/tesseract/neoforge/compat/vanilla/material/recipe/VanillaMaterialRecipeGroups.java
+++ src/main/java/net/swedz/tesseract/neoforge/compat/vanilla/material/recipe/VanillaMaterialRecipeGroups.java
@@ -2,11 +2,16 @@ package net.swedz.tesseract.neoforge.compat.vanilla.material.recipe;
 
 import net.swedz.tesseract.neoforge.material.recipe.MaterialRecipeGroup;
 
+import static net.swedz.tesseract.neoforge.compat.vanilla.material.part.VanillaMaterialParts.*;
+
 public interface VanillaMaterialRecipeGroups
 {
+	MaterialRecipeGroup STANDARD = new MaterialRecipeGroup()
+			.add("nugget_to_ingot", (context) -> context.full3x3(NUGGET, INGOT, true))
+			.add("main_to_block", (context) -> context.full3x3(context.mainPart(), BLOCK, true))
+			.add("raw_metal_to_block", (context) -> context.full3x3(RAW_METAL, RAW_METAL_BLOCK, true));
+	
 	MaterialRecipeGroup SMELTING = new MaterialRecipeGroup()
-			.add("raw_metal_to_primary", (context) ->
-			{
-				// TODO
-			});
+			.add("raw_metal_to_primary", (context) -> context.smeltingAndBlasting(RAW_METAL, context.mainPart()));
+	// TODO ores
 }
