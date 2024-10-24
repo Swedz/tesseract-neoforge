d8b7d6d68533b528969e752b750606a36f2c793c
Swedz <me@swedz.net>
2024-10-23 22:33:34 -0400

    Include context type in recipe group field

--- src/main/java/net/swedz/tesseract/neoforge/material/builtin/recipe/VanillaMaterialRecipeGroups.java
+++ src/main/java/net/swedz/tesseract/neoforge/material/builtin/recipe/VanillaMaterialRecipeGroups.java
@@ -6,12 +6,12 @@ import static net.swedz.tesseract.neoforge.material.builtin.part.MaterialParts.*
 
 public interface VanillaMaterialRecipeGroups
 {
-	MaterialRecipeGroup STANDARD = MaterialRecipeGroup.create(VanillaMaterialRecipeContext::new)
+	MaterialRecipeGroup<VanillaMaterialRecipeContext> STANDARD = MaterialRecipeGroup.create(VanillaMaterialRecipeContext::new)
 			.add("nugget_to_ingot", (context) -> context.shapeless3x3(NUGGET, INGOT, true))
 			.add("main_to_block", (context) -> context.shapeless3x3(context.mainPart(), BLOCK, true))
 			.add("raw_metal_to_block", (context) -> context.shapeless3x3(RAW_METAL, RAW_METAL_BLOCK, true));
 	
-	MaterialRecipeGroup SMELTING = MaterialRecipeGroup.create(VanillaMaterialRecipeContext::new)
+	MaterialRecipeGroup<VanillaMaterialRecipeContext> SMELTING = MaterialRecipeGroup.create(VanillaMaterialRecipeContext::new)
 			.add("raw_metal_to_ingot", (context) -> context.smeltingAndBlasting(RAW_METAL, INGOT, 0.7f))
 			.add("ore_to_primary", (context) -> context.smeltingAndBlasting(ORE, context.mainPart(), 0.7f))
 			.add("ore_deepslate_to_primary", (context) -> context.smeltingAndBlasting(ORE_DEEPSLATE, context.mainPart(), 0.7f))
