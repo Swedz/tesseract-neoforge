7998be676125aa77a13440b35bd3549d843c4438
Swedz <me@swedz.net>
2024-11-18 00:48:26 -0500

    Add hasAny methods for material part holders

--- src/main/java/net/swedz/tesseract/neoforge/material/part/MaterialPartHolder.java
+++ src/main/java/net/swedz/tesseract/neoforge/material/part/MaterialPartHolder.java
@@ -2,9 +2,22 @@ package net.swedz.tesseract.neoforge.material.part;
 
 import net.swedz.tesseract.neoforge.material.Material;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface MaterialPartHolder
 {
 	boolean has(MaterialPart part);
+	
+	default boolean hasAny(Collection<MaterialPart> parts)
+	{
+		return parts.stream().anyMatch(this::has);
+	}
+	
+	default boolean hasAny(MaterialPart... parts)
+	{
+		return this.hasAny(List.of(parts));
+	}
 	
 	RegisteredMaterialPart get(MaterialPart part);
 	
