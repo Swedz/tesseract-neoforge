fe4bc0c892733ab91c0400d5d85d46b252464abc
Swedz <me@swedz.net>
2024-12-12 07:08:37 -0500

    Add methods for broadcasting packets to clients (#42)

--- src/main/java/net/swedz/tesseract/neoforge/packet/CustomPacket.java
+++ src/main/java/net/swedz/tesseract/neoforge/packet/CustomPacket.java
@@ -1,8 +1,13 @@
 package net.swedz.tesseract.neoforge.packet;
 
+import net.minecraft.core.Position;
+import net.minecraft.core.Vec3i;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.neoforge.network.PacketDistributor;
+
+import java.util.function.Predicate;
 
 public interface CustomPacket extends CustomPacketPayload
 {
@@ -16,6 +21,35 @@ public interface CustomPacket extends CustomPacketPayload
 	default void sendToClient(ServerPlayer player)
 	{
 		PacketDistributor.sendToPlayer(player, this);
+	}
+	
+	default int broadcastToClients(ServerLevel level, Predicate<ServerPlayer> predicate)
+	{
+		int sent = 0;
+		for(ServerPlayer player : level.players())
+		{
+			if(predicate.test(player))
+			{
+				this.sendToClient(player);
+				sent++;
+			}
+		}
+		return sent;
+	}
+	
+	default int broadcastToClients(ServerLevel level)
+	{
+		return this.broadcastToClients(level, (player) -> true);
+	}
+	
+	default int broadcastToClients(ServerLevel level, Position origin, double distance)
+	{
+		return this.broadcastToClients(level, (player) -> player.blockPosition().closerToCenterThan(origin, distance));
+	}
+	
+	default int broadcastToClients(ServerLevel level, Vec3i origin, double distance)
+	{
+		return this.broadcastToClients(level, (player) -> player.blockPosition().closerThan(origin, distance));
 	}
 	
 	Type<? extends CustomPacket> type();
