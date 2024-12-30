58114d2278df6015ca6d8d264542c1bcef7d5b5a
Swedz <me@swedz.net>
2024-12-29 23:26:47 -0500

    Add buildInventoryComponent helper for machines (#55)

--- src/main/java/net/swedz/tesseract/neoforge/compat/mi/helper/MachineInventoryHelper.java
+++ src/main/java/net/swedz/tesseract/neoforge/compat/mi/helper/MachineInventoryHelper.java
@@ -1,17 +1,57 @@
 package net.swedz.tesseract.neoforge.compat.mi.helper;
 
+import aztech.modern_industrialization.MIFluids;
 import aztech.modern_industrialization.definition.FluidLike;
 import aztech.modern_industrialization.inventory.ConfigurableFluidStack;
 import aztech.modern_industrialization.inventory.ConfigurableItemStack;
+import aztech.modern_industrialization.inventory.SlotPositions;
+import aztech.modern_industrialization.machines.components.MachineInventoryComponent;
 import aztech.modern_industrialization.util.Simulation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.material.Fluid;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class MachineInventoryHelper
 {
+	public static MachineInventoryComponent buildInventoryComponent(
+			int itemInputCount, int itemOutputCount, int fluidInputCount, int fluidOutputCount,
+			SlotPositions itemPositions, SlotPositions fluidPositions, int steamBuckets, int ioBucketCapacity
+	)
+	{
+		List<ConfigurableItemStack> itemInputStacks = new ArrayList<>();
+		for(int i = 0; i < itemInputCount; ++i)
+		{
+			itemInputStacks.add(ConfigurableItemStack.standardInputSlot());
+		}
+		
+		List<ConfigurableItemStack> itemOutputStacks = new ArrayList<>();
+		for(int i = 0; i < itemOutputCount; ++i)
+		{
+			itemOutputStacks.add(ConfigurableItemStack.standardOutputSlot());
+		}
+		
+		List<ConfigurableFluidStack> fluidInputStacks = new ArrayList<>();
+		if(steamBuckets > 0)
+		{
+			fluidInputStacks.add(ConfigurableFluidStack.lockedInputSlot(1000L * steamBuckets, MIFluids.STEAM.asFluid()));
+		}
+		for(int i = 0; i < fluidInputCount; ++i)
+		{
+			fluidInputStacks.add(ConfigurableFluidStack.standardInputSlot(1000L * ioBucketCapacity));
+		}
+		
+		List<ConfigurableFluidStack> fluidOutputStacks = new ArrayList<>();
+		for(int i = 0; i < fluidOutputCount; ++i)
+		{
+			fluidOutputStacks.add(ConfigurableFluidStack.standardOutputSlot(1000L * ioBucketCapacity));
+		}
+		
+		return new MachineInventoryComponent(itemInputStacks, itemOutputStacks, fluidInputStacks, fluidOutputStacks, itemPositions, fluidPositions);
+	}
+	
 	public static boolean isActuallyJustAir(ConfigurableItemStack item)
 	{
 		return item.isLockedTo(Items.AIR) || (item.getLockedInstance() == null && item.isEmpty());
