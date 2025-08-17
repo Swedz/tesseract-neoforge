f9a006272cf81d54a3a0cb2b52f2b7acf106058d
Swedz <me@swedz.net>
2025-08-17 00:53:54 -0400

    Add accessor for the active recipe of an MI CrafterComponent

--- src/main/java/net/swedz/tesseract/neoforge/compat/mi/mixin/accessor/CrafterComponentAccessor.java
+++ src/main/java/net/swedz/tesseract/neoforge/compat/mi/mixin/accessor/CrafterComponentAccessor.java
@@ -1,7 +1,9 @@
 package net.swedz.tesseract.neoforge.compat.mi.mixin.accessor;
 
 import aztech.modern_industrialization.machines.components.CrafterComponent;
+import aztech.modern_industrialization.machines.recipe.MachineRecipe;
 import aztech.modern_industrialization.machines.recipe.condition.MachineProcessCondition;
+import net.minecraft.world.item.crafting.RecipeHolder;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
@@ -11,6 +13,9 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 )
 public interface CrafterComponentAccessor
 {
+	@Accessor("activeRecipe")
+	RecipeHolder<MachineRecipe> getActiveRecipe();
+	
 	@Accessor("conditionContext")
 	MachineProcessCondition.Context getConditionContext();
 }
