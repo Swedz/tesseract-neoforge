fedfce0a7364e7b3dec2ef156d7e361a9268c9e5
Swedz <me@swedz.net>
2025-08-22 06:36:41 -0700

    Fix 3x3 compacting recipes being shapeless (#119)

--- src/main/java/net/swedz/tesseract/neoforge/material/builtin/recipe/VanillaMaterialRecipeGroups.java
+++ src/main/java/net/swedz/tesseract/neoforge/material/builtin/recipe/VanillaMaterialRecipeGroups.java
@@ -7,9 +7,9 @@ import static net.swedz.tesseract.neoforge.material.builtin.part.MaterialParts.*
 public interface VanillaMaterialRecipeGroups
 {
 	MaterialRecipeGroup<VanillaMaterialRecipeContext> STANDARD = MaterialRecipeGroup.create(VanillaMaterialRecipeContext::new)
-			.add("nugget_to_ingot", (context) -> context.shapeless3x3(NUGGET, INGOT, true))
-			.add("main_to_block", (context) -> context.shapeless3x3(context.mainPart(), BLOCK, true))
-			.add("raw_metal_to_block", (context) -> context.shapeless3x3(RAW_METAL, RAW_METAL_BLOCK, true));
+			.add("nugget_to_ingot", (context) -> context.compacting(NUGGET, INGOT, true))
+			.add("main_to_block", (context) -> context.compacting(context.mainPart(), BLOCK, true))
+			.add("raw_metal_to_block", (context) -> context.compacting(RAW_METAL, RAW_METAL_BLOCK, true));
 	
 	MaterialRecipeGroup<VanillaMaterialRecipeContext> SMELTING = MaterialRecipeGroup.create(VanillaMaterialRecipeContext::new)
 			.add("raw_metal_to_ingot", (context) -> context.smeltingAndBlasting(RAW_METAL, INGOT, 0.7f))
