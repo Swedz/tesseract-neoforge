0e8c8a3c639716cf030b56984a13cca1231ca5ba
Swedz <me@swedz.net>
2025-12-09 17:51:25 -0500

    Add methods to MachineBuiltinModelBuilder for MI's new output textures

--- src/main/java/net/swedz/tesseract/neoforge/compat/mi/machine/builder/MachineBuiltinModelBuilder.java
+++ src/main/java/net/swedz/tesseract/neoforge/compat/mi/machine/builder/MachineBuiltinModelBuilder.java
@@ -1,6 +1,8 @@
 package net.swedz.tesseract.neoforge.compat.mi.machine.builder;
 
+import aztech.modern_industrialization.MI;
 import aztech.modern_industrialization.machines.models.MachineCasing;
+import net.minecraft.resources.ResourceLocation;
 import net.swedz.tesseract.neoforge.api.Assert;
 import net.swedz.tesseract.neoforge.compat.mi.hack.HackedMachineRegistrationHelper;
 import net.swedz.tesseract.neoforge.compat.mi.hook.MIHook;
@@ -11,6 +13,8 @@ public class MachineBuiltinModelBuilder
 	private final String        overlayFolder;
 	
 	private boolean front, top, side, active;
+	
+	private String outputTexture;
 	
 	MachineBuiltinModelBuilder(MachineCasing casing, String overlayFolder)
 	{
@@ -63,10 +67,41 @@ public class MachineBuiltinModelBuilder
 		return this.active(true);
 	}
 	
+	public MachineBuiltinModelBuilder outputTexture(String texture)
+	{
+		this.outputTexture = texture;
+		return this;
+	}
+	
+	public MachineBuiltinModelBuilder outputTexture(ResourceLocation texture)
+	{
+		return this.outputTexture(texture == null ? null : texture.toString());
+	}
+	
+	public MachineBuiltinModelBuilder outputTextureDefault()
+	{
+		return this.outputTexture((String) null);
+	}
+	
+	public MachineBuiltinModelBuilder outputTextureItem()
+	{
+		return this.outputTexture(MI.id("block/overlays/output_item"));
+	}
+	
+	public MachineBuiltinModelBuilder outputTextureFluid()
+	{
+		return this.outputTexture(MI.id("block/overlays/output_fluid"));
+	}
+	
+	public MachineBuiltinModelBuilder outputTextureEnergy()
+	{
+		return this.outputTexture(MI.id("block/overlays/output_energy"));
+	}
+	
 	void build(MIHook hook, String id, MachineCasing casing)
 	{
 		Assert.notNull(casing, "Machine casing must be provided");
-		HackedMachineRegistrationHelper.addMachineModel(hook, id, casing, overlayFolder, front, top, side, active);
+		HackedMachineRegistrationHelper.addMachineModel(hook, id, casing, overlayFolder, front, top, side, active, outputTexture);
 	}
 	
 	void build(MIHook hook, String id)
