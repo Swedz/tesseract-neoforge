a5412a42c0852211a33bca1b9e0999cec9d5a697
Swedz <me@swedz.net>
2026-02-01 10:14:25 -0500

    Add ParsedDecimal annotation in text API for specifying decimal places of float and double values (#197)

---
+++ src/main/java/net/swedz/tesseract/neoforge/lang/parser/parser/DecimalParser.java
@@ -0,0 +1,30 @@
+package net.swedz.tesseract.neoforge.lang.parser.parser;
+
+import net.minecraft.network.chat.Component;
+import net.swedz.tesseract.neoforge.api.Assert;
+import net.swedz.tesseract.neoforge.lang.LangContext;
+import net.swedz.tesseract.neoforge.lang.annotation.ParsedDecimal;
+import net.swedz.tesseract.neoforge.lang.exception.MalformedParserException;
+import net.swedz.tesseract.neoforge.lang.parser.ParserProvider;
+
+import java.util.Locale;
+
+public final class DecimalParser<T> implements ParserProvider<T>
+{
+	@Override
+	public Component parse(LangContext context, T value)
+	{
+		if(context.hasAnnotation(ParsedDecimal.class))
+		{
+			var annotation = context.getAnnotation(ParsedDecimal.class);
+			var decimalPlaces = annotation.value();
+			
+			Assert.that(decimalPlaces >= 0, "ParsedDecimal value must be >= 0", MalformedParserException::new);
+			
+			var format = "%." + decimalPlaces + "f";
+			var parsedString = String.format(Locale.ROOT, format, value);
+			return Component.literal(parsedString);
+		}
+		return Component.literal(String.valueOf(value));
+	}
+}
