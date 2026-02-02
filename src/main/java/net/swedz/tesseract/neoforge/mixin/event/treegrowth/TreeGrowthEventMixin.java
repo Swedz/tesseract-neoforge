16af1d5063acf999d70d495ef6e80babfc97fa72
Swedz <me@swedz.net>
2026-02-01 22:19:21 -0500

    Fix crash when other mod assumes WorldGenLevel is Level (#198)

--- src/main/java/net/swedz/tesseract/neoforge/mixin/event/treegrowth/TreeGrowthEventMixin.java
+++ src/main/java/net/swedz/tesseract/neoforge/mixin/event/treegrowth/TreeGrowthEventMixin.java
@@ -1,48 +1,62 @@
 package net.swedz.tesseract.neoforge.mixin.event.treegrowth;
 
+import com.google.common.collect.Sets;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.WorldGenRegion;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.neoforged.neoforge.common.NeoForge;
-import net.swedz.tesseract.neoforge.event.treegrowth.TrackingWorldGenLevel;
 import net.swedz.tesseract.neoforge.event.treegrowth.TreeGrowthEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Set;
 
 @Mixin(Feature.class)
 public class TreeGrowthEventMixin
 {
-	@ModifyArg(
+	@Inject(
 			method = "place(Lnet/minecraft/world/level/levelgen/feature/configurations/FeatureConfiguration;Lnet/minecraft/world/level/WorldGenLevel;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/util/RandomSource;Lnet/minecraft/core/BlockPos;)Z",
 			at = @At(
 					value = "INVOKE",
-					target = "Lnet/minecraft/world/level/levelgen/feature/FeaturePlaceContext;<init>(Ljava/util/Optional;Lnet/minecraft/world/level/WorldGenLevel;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/util/RandomSource;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/levelgen/feature/configurations/FeatureConfiguration;)V"
+					target = "Lnet/minecraft/world/level/levelgen/feature/Feature;place(Lnet/minecraft/world/level/levelgen/feature/FeaturePlaceContext;)Z"
 			)
 	)
-	private WorldGenLevel place(WorldGenLevel level,
-								@Local(argsOnly = true) FeatureConfiguration configuration,
-								@Share("trackingLevel") LocalRef<TrackingWorldGenLevel> trackingLevelReference)
+	private void beforePlace(
+			FeatureConfiguration config,
+			WorldGenLevel level,
+			ChunkGenerator chunkGenerator,
+			RandomSource random,
+			BlockPos origin,
+			CallbackInfoReturnable<Boolean> callback,
+			@Local(argsOnly = true) FeatureConfiguration configuration,
+			@Share("actualLevel") LocalRef<Level> actualLevelReference,
+			@Share("trackedBlockChanges") LocalRef<Set<BlockPos>> trackedBlockChangesReference
+	)
 	{
-		if(!(level instanceof WorldGenRegion))
+		if(level instanceof Level actualLevel)
 		{
 			var feature = (Feature) (Object) this;
 			if(TreeGrowthEvent.shouldPostEventFor(feature, configuration))
 			{
-				var trackingLevel = new TrackingWorldGenLevel(level);
-				trackingLevelReference.set(trackingLevel);
-				return trackingLevel;
+				actualLevelReference.set(actualLevel);
+				
+				Set<BlockPos> trackedBlockChanges = Sets.newConcurrentHashSet();
+				trackedBlockChangesReference.set(trackedBlockChanges);
+				actualLevel.startTrackingBlockChanges(trackedBlockChanges);
 			}
 		}
-		return level;
 	}
 	
 	@WrapOperation(
@@ -52,30 +66,39 @@ public class TreeGrowthEventMixin
 					target = "Lnet/minecraft/world/level/levelgen/feature/Feature;place(Lnet/minecraft/world/level/levelgen/feature/FeaturePlaceContext;)Z"
 			)
 	)
-	private boolean place(
+	private boolean afterPlace(
 			Feature instance,
 			FeaturePlaceContext<FeatureConfiguration> context,
 			Operation<Boolean> original,
 			@Local(argsOnly = true) WorldGenLevel level,
 			@Local(argsOnly = true) BlockPos origin,
-			@Share("trackingLevel") LocalRef<TrackingWorldGenLevel> trackingLevelReference
+			@Share("actualLevel") LocalRef<Level> actualLevelReference,
+			@Share("trackedBlockChanges") LocalRef<Set<BlockPos>> trackedBlockChangesReference
 	)
 	{
 		boolean result = original.call(instance, context);
+		
+		var actualLevel = actualLevelReference.get();
+		if(actualLevel != null)
+		{
+			actualLevel.stopTrackingBlockChanges();
+		}
+		
 		if(result)
 		{
-			var trackingLevel = trackingLevelReference.get();
-			if(trackingLevel != null)
+			var trackedBlockChanges = trackedBlockChangesReference.get();
+			if(trackedBlockChanges != null)
 			{
 				var event = new TreeGrowthEvent(
 						level,
 						origin,
 						level.getBlockState(origin),
-						trackingLevel.getModifiedBlockPositions()
+						trackedBlockChanges
 				);
 				NeoForge.EVENT_BUS.post(event);
 			}
 		}
+		
 		return result;
 	}
 }
