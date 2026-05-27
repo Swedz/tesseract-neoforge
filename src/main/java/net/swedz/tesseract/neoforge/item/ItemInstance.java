0d181794d34aeaeee88aec2f6fcb6321ede6de84
Swedz <me@swedz.net>
2026-05-27 05:20:13 -0400

    Use better handling of count in ItemInstance

--- src/main/java/net/swedz/tesseract/neoforge/item/ItemInstance.java
+++ src/main/java/net/swedz/tesseract/neoforge/item/ItemInstance.java
@@ -1,5 +1,6 @@
 package net.swedz.tesseract.neoforge.item;
 
+import com.google.common.collect.Lists;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Holder;
@@ -11,7 +12,11 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
+import net.swedz.tesseract.api.Assert;
 import net.swedz.tesseract.neoforge.helper.CodecHelper;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 public record ItemInstance(
 		Holder<Item> item,
@@ -52,6 +57,29 @@ public record ItemInstance(
 	
 	public ItemStack asStack()
 	{
+		if(count <= 0)
+		{
+			return ItemStack.EMPTY;
+		}
+		Assert.that(count <= 99, "Count must be less than 100 to create an ItemStack");
 		return new ItemStack(item, count, components);
+	}
+	
+	public Stream<ItemStack> asStacks()
+	{
+		List<ItemStack> stacks = Lists.newArrayList();
+		int remaining = count;
+		while(remaining > 0)
+		{
+			int stackCount = Math.min(64, remaining);
+			var stack = new ItemStack(item, stackCount);
+			if(!components.isEmpty())
+			{
+				stack.applyComponents(components);
+			}
+			stacks.add(stack);
+			remaining -= stackCount;
+		}
+		return stacks.stream();
 	}
 }
