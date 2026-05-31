947083e2de667f5c42828f5dd1e75e48a3d9b762
Swedz <me@swedz.net>
2026-05-30 22:29:49 -0400

    Well, that was a mistake

--- src/main/java/net/swedz/tesseract/neoforge/helper/datagen/BlockAndItemModelGeneratorsByIdentifier.java
+++
@@ -1,9 +0,0 @@
-package net.swedz.tesseract.neoforge.helper.datagen;
-
-public interface BlockAndItemModelGeneratorsByIdentifier
-{
-	default SimpleModelCollectorByIdentifier modelOutputById()
-	{
-		throw new UnsupportedOperationException();
-	}
-}
